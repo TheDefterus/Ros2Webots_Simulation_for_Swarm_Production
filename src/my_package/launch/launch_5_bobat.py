5672343ff08bafad9767a8887ecd9dd76749c3ae
kingapiper <kingapiperb@gmail.com>
2026-01-28 20:57:55 +0100

    Launch stuff together

--- src/my_package/launch/launch_5_bobat.py
+++ src/my_package/launch/launch_5_bobat.py
@@ -1,19 +1,25 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, OpaqueFunction
+from launch.actions import IncludeLaunchDescription, OpaqueFunction, DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 
 
 def launch_setup(context):
     package_dir = get_package_share_directory("my_package")
+    number = 5
     launches = []
-    for i in range(5):
-        bobat_name = 'bobat_{}'.format(i+1)
-        launches.append(
-            IncludeLaunchDescription(package_dir + "/launch/" + "bobat_number_launch.py", launch_arguments={
-            'bobat_namespace': bobat_name,
-            }.items())
-            )
-    launches.append(IncludeLaunchDescription(package_dir + "/launch/" + "many_bots_launch.py"))
+    launches.append(
+        IncludeLaunchDescription(
+            package_dir + "/launch/" + "test_launch.py",
+        )
+    )
+    launches.append(
+        IncludeLaunchDescription(
+            package_dir + "/launch/" + "launch_any_bobat.py",
+            launch_arguments={
+                'number': str(number),
+            }.items(),
+        )
+    )
     return launches
 
 def generate_launch_description():
